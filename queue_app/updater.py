0d3bfcbb7491f96198e5251335e0394ba5caf43f
mcglow2-RPI <mcglow2@rpi.edu>
2017-09-13 09:54:31 -0400

    Switched to LPQ

--- queue_app/updater.py
+++ queue_app/updater.py
@@ -2,13 +2,14 @@ import paramiko
 import json
 import time
 import subprocess
+import sys
 from . import app
 
 def update_printer():
     # Initialize Paramiko, get information from config file
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(app.config['SSH_SERVER'], username = app.config['SSH_USERNAME'], password = app.config['SSH_PASSWORD'])
+    # ssh = paramiko.SSHClient()
+    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # ssh.connect(app.config['SSH_SERVER'], username = app.config['SSH_USERNAME'], password = app.config['SSH_PASSWORD'])
     
     # Get the initial printer data from "printers_init.json"
     with open('queue_app/static/json/printers_init.json', 'r') as json_printer:
@@ -20,9 +21,14 @@ def update_printer():
             stdin, stdout, stderr = ssh.exec_command("lpq -P" + printer['name'])
             output = stdout.readlines()
         elif printer['method'] == 'rlpq':
-            proc = subprocess.Popen(["rlpq", "-N", "-H", app.config['SERVER'], "-P" + printer['name']], stdout=subprocess.PIPE).communicate()[0]
+            if 'server' in printer.keys():
+                if printer['server'] != '':
+                    server = printer['server']
+            
+            proc = subprocess.check_output(["rlpq", "-H", server, "-P" + printer['name'] , "-N" ], stderr=subprocess.STDOUT)
             output = proc.decode('utf-8').split('\n')
-		
+
+        print "Checking server:" + server + " queue: " +  printer['name']
 
         # State Guide:
         # 1 - No Entries
@@ -38,6 +44,9 @@ def update_printer():
             elif 'printer not found' in output[0].lower():
                 printer['state'] = 4
                 printer['error'] = "The printer is currently not available."
+            elif 'Malformed from address' in output[0].lower():
+                printer['state'] = 3
+                printer['error'] = "Access to print server denied."
 
         elif len(output) >= 1:
             # Busy = it's printing
@@ -61,8 +70,8 @@ def update_printer():
             printer['error'] = "Printer did not respond."
 
     # Command to clean some cache file that appear after running "lpq"
-    ssh.exec_command("lpq-pqtest --kdest")  
-    ssh.close()
+    # ssh.exec_command("lpq-pqtest --kdest")  
+    # ssh.close()
 
     # Update the timestamp
     printer_data['last_updated'] = time.strftime("%a, %b %d, %I:%M %p")
@@ -73,7 +82,7 @@ def update_printer():
 
     json_printer.close()
 
-def parse_output(printer, output):
+def parse_output_ssh(printer, output):
     # Find the index where the jobs actually start to be listed
     jobs = ''
     for i, line in enumerate(output):
@@ -89,6 +98,79 @@ def parse_output(printer, output):
     for job in jobs:
         info = job.split()
         queue.append({ 'queue_pos':info[0],'user_id':info[1],'job_num':info[2],'filename':''.join(info[3:-2]) })
+
+    printer['queue'] = queue
+
+def parse_output(printer, output):
+    if 'Windows LPD Server' in str(output):
+        return parse_output_windows(printer, output)
+    else:
+        return parse_output_rcs(printer, output)
+
+
+def parse_output_rcs(printer, output):
+    print "parsing for RCS format"
+    # Find the index where the jobs actually start to be listed
+    jobs = ''
+    for i, line in enumerate(output):
+        if line.lower().split() == ['rank', 'owner', 'job', 'files', 'total', 'size']:
+            j = i + 1
+            jobs = output[j:]
+
+    # Can't find the list to parse, stop parsing
+    if jobs == '':
+        return
+
+    queue = []
+    for job in jobs:
+        if job == '':
+            continue
+        if job == 'no entries':
+            continue
+
+        try:
+            #print job
+            info = job.split()
+            queue.append({'queue_pos': info[0], 'user_id': info[1], 'job_num': info[2], 'filename': ''.join(info[3:-2])})
+
+        except  (RuntimeError, TypeError, NameError, IndexError):
+            print "Error with parse: " + job
+
+    printer['queue'] = queue
+
+
+def parse_output_windows(printer, output):
+    print "parsing for Windows LPD format"
+    # Find the index where the jobs actually start to be listed
+    jobs = ''
+    for i, line in enumerate(output):
+        if line.lower().split() == ['owner', 'status', 'jobname', 'job-id', 'size', 'pages', 'priority']:
+            j = i + 1
+            jobs = output[j:]
+
+    # Can't find the list to parse, stop parsing
+    if jobs == '':
+        return
+
+    queue = []
+    count = 1
+    for job in jobs:
+        if job == '':
+            continue
+        if job.replace('-','') == '':
+            continue
+        if job == 'no entries':
+            continue
+        if job is None:
+            continue
+
+        try:
+            #print job
+            info = job.split()
+            queue.append({'queue_pos': count, 'user_id': str(info[0]).lower(), 'job_num': info[3], 'filename': ' '.join(info[2:-4])})
+            count = count + 1
+        except  (RuntimeError, TypeError, NameError, IndexError):
+            print "Error with parse: " + job
 
     printer['queue'] = queue
 
