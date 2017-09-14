8ef4df1eaf4febd0a5478f0ca9ddc885a6672d5d
mcglow2-RPI <mcglow2@rpi.edu>
2017-09-14 09:41:17 -0400

    Additionial clean up for rlpq switch
    Additionial clean up for rlpq switch removing all old SSH code

--- queue_app/updater.py
+++ queue_app/updater.py
@@ -1,4 +1,3 @@
-import paramiko
 import json
 import time
 import subprocess
@@ -6,28 +5,20 @@ import sys
 from . import app
 
 def update_printer():
-    # Initialize Paramiko, get information from config file
-    # ssh = paramiko.SSHClient()
-    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # ssh.connect(app.config['SSH_SERVER'], username = app.config['SSH_USERNAME'], password = app.config['SSH_PASSWORD'])
     
     # Get the initial printer data from "printers_init.json"
-    with open('queue_app/static/json/printers_init.json', 'r') as json_printer:
+    with open( BASE_PATH + 'queue_app/static/json/printers_init.json', 'r') as json_printer:
         printer_data = json.load(json_printer)
     
     for printer in printer_data['printers']:
 
-        if printer['method'] == 'ssh':
-            stdin, stdout, stderr = ssh.exec_command("lpq -P" + printer['name'])
-            output = stdout.readlines()
-        elif printer['method'] == 'rlpq':
-            server = app.config['DEFAULT_LPQ_SERVER']
-            if 'server' in printer.keys():
-                if printer['server'] != '':
-                    server = printer['server']
-            
-            proc = subprocess.check_output(["rlpq", "-H", server, "-P" + printer['name'] , "-N" ], stderr=subprocess.STDOUT)
-            output = proc.decode('utf-8').split('\n')
+        server = app.config['DEFAULT_LPQ_SERVER']
+        if 'server' in printer.keys():
+            if printer['server'] != '':
+                server = printer['server']
+
+        proc = subprocess.check_output(["rlpq", "-H", server, "-P" + printer['name'] , "-N" ], stderr=subprocess.STDOUT)
+        output = proc.decode('utf-8').split('\n')
 
         print "Checking server:" + server + " queue: " +  printer['name']
 
@@ -70,37 +61,14 @@ def update_printer():
             printer['state'] = 4
             printer['error'] = "Printer did not respond."
 
-    # Command to clean some cache file that appear after running "lpq"
-    # ssh.exec_command("lpq-pqtest --kdest")  
-    # ssh.close()
-
     # Update the timestamp
     printer_data['last_updated'] = time.strftime("%a, %b %d, %I:%M %p")
 
     # Dump all the data into a new JSON file named "printers_data.json"
-    with open('queue_app/static/json/printers_data.json', 'w') as write_printer:
+    with open(BASE_PATH +'queue_app/static/json/printers_data.json', 'w') as write_printer:
         json.dump(printer_data, write_printer, indent=4, sort_keys=True)
 
     json_printer.close()
-
-def parse_output_ssh(printer, output):
-    # Find the index where the jobs actually start to be listed
-    jobs = ''
-    for i, line in enumerate(output):
-        if line.lower().split() == ['rank', 'owner', 'job', 'files', 'total', 'size']:
-            j = i + 1
-            jobs = output[j:]
-
-    # Can't find the list to parse, stop parsing
-    if jobs == '':
-        return
-
-    queue = []
-    for job in jobs:
-        info = job.split()
-        queue.append({ 'queue_pos':info[0],'user_id':info[1],'job_num':info[2],'filename':''.join(info[3:-2]) })
-
-    printer['queue'] = queue
 
 def parse_output(printer, output):
     if 'Windows LPD Server' in str(output):
